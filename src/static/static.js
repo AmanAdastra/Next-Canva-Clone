5e2c8ae52778825c8bd29f9fd5856224df6b1ca1
Aman Khan <amanayankhan78@gmail.com>
2023-10-02 11:41:41 +0530

    Create static.js

---
+++ src/static/static.js
@@ -0,0 +1,15 @@
+const carousalpiclinks = [
+    "https://static.canva.com/web/images/fc97fa19d2f66f7d4ea882bc9eb7408b.jpg",
+    "https://static.canva.com/web/images/de8f4c428ed0f12d187407121929abf7.jpg",
+    "https://static.canva.com/web/images/ef5d750875ce5256c0467a5b1af7848d.jpg",
+    "https://static.canva.com/web/images/5d85a01eef893b352ae26f29f48a1fdd.jpg",
+    "https://static.canva.com/web/images/ec12999da1144aa1345e82a4e141f53b.png",
+    "https://static.canva.com/web/images/5f9d0a043ec48e8cd4ebf53ec126c260.jpg",
+    "https://static.canva.com/web/images/b34374310f36cd5ad2b235fa7d0a8536.jpg",
+    "https://static.canva.com/web/images/2854ded8fdeba11a39394e4c5feff34a.jpg",
+    "https://static.canva.com/web/images/172daa2c3afefcef19577439c081c52a.jpg",
+    "https://static.canva.com/web/images/8e4c8fc1c9131aedee1176608db7b2d5.jpg"
+
+]
+
+export {carousalpiclinks}
